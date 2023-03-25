0d390d83bcf541161c414b45d212afcfe4893cf6
Edelson Silva <edelson_2014@hotmail.com>
2023-03-25 17:02:01 -0300

    modificação na estrutura do projeto

--- APPnodeJS/cultoparacasais/src/js/validations.js
+++
@@ -1,4 +0,0 @@
-export function validateEmail(email){
-    const emailRegex = new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/, "gm");
-    return emailRegex.test(email);
-}
--- app.py
+++ app.py
@@ -1,4 +1,61 @@
-from flaskr import create_app
+import os
+import db
+import auth
+import bingo
+from flask import Flask
+# from .utils.momentjs import *
 
 
-app = create_app('__main__')
+def create_app(test_config=None):
+    # create and configure the app Flask
+    # app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_path=os.path.abspath('./instance'))
+    # app.jinja_env.globals['momentjs'] = momentjs
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    
+    db.init_app(app)
+
+    
+    app.register_blueprint(auth.bp)
+
+    # from . import blog
+    # app.register_blueprint(blog.bp)
+
+    
+    app.register_blueprint(bingo.bp)
+    app.add_url_rule('/', endpoint='index')
+
+    return app.run(debug=True, port=5000)
+
+
+if __name__ == '__main__':
+    create_app()
+
+# export FLASK_APP=flaskr
+# export FLASK_ENV=development
+# flask run -p 8900
+
+# flask --app flaskr init-db
+# flask --app flaskr --debug run --port 8900
